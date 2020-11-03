19992d563a05e323b535f9b16533972f8e7b39a7
John Martin <jlemartin7@gmail.com>
2020-11-03 11:48:08 -0600

    working test

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+        }
+
+        // A private field of type double named balance with a value of 0
+        private double balance = 0;
+
+        // Define a method named Deposit that will accept a double and store that value in the balance field
+        public void Deposit(double deposit)
+        {
+            balance += deposit;
+        }
+
+        // Define a method named GetBalance that will return the amount stored in the balance field
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a new instance of the BankAccount class
+            var account1 = new BankAccount();
+
+            Console.WriteLine($"Current balance: {account1.GetBalance()}");
+
+            // Allow the user of the application to Deposit money
+            Console.Write("Enter deposit amount:");
+            var amount = Convert.ToDouble(Console.ReadLine());
+            account1.Deposit(amount);
+
+            // and retrieve their balance through the console
+            Console.WriteLine($"New balance: {account1.GetBalance()}");
+
+
+        }
+    }
+}
