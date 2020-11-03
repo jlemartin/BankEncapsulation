d2314ca390bed5356eadb70d3c8ecb7b1ed7a14e
John Martin <jlemartin7@gmail.com>
2020-11-03 12:00:06 -0600

    final refinements

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -9,22 +9,22 @@ namespace BankEncapsulation
             // create a new instance of the BankAccount class
             var account1 = new BankAccount();
 
-            Console.WriteLine($"Current balance: {account1.GetBalance()}");
+            Console.WriteLine($"Current balance: ${account1.GetBalance()}");
 
             // Allow the user of the application to Deposit money
             Console.Write("Enter deposit amount: ");
-            var amount = Convert.ToDouble(Console.ReadLine());
-            account1.Deposit(amount);
+            var amountToDeposit = Convert.ToDouble(Console.ReadLine());
+            account1.Deposit(amountToDeposit);
 
             // and retrieve their balance through the console
-            Console.WriteLine($"New balance: {account1.GetBalance()}");
+            Console.WriteLine($"New balance: ${account1.GetBalance()}");
 
             // making another deposit
             Console.Write("Enter another deposit amount: ");
-            amount = Convert.ToDouble(Console.ReadLine());
-            account1.Deposit(amount);
+            amountToDeposit = Convert.ToDouble(Console.ReadLine());
+            account1.Deposit(amountToDeposit);
 
-            Console.WriteLine($"New balance: {account1.GetBalance()}");
+            Console.WriteLine($"Updated balance: ${account1.GetBalance()}");
         }
     }
 }
